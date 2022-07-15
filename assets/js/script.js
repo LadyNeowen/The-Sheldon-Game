cef45d76050256ec2b8d7c1ac499181a4e4c97ba
LadyNeowen <Alicia.Westerlund@outlook.com>
2022-07-15 22:20:26 +0000

    Added additional tweaking and style to the image choices and the score section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,83 @@
-// Wait for the DOM to finish loading before running the game
-// Get the button element and add event liseners to it
+var you;
+var yourScore = 0;
+var computer;
+var computerScore = 0;
+
+var choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+window.onload = function () {
+    for (let i = 0; i < 5; i++) {
+        let choice = document.createElement('img');
+        choice.id = choices[i];
+        choice.src = choices[i] + '.jpg';
+        choice.addEventListener('click', selectChoice);
+        document.getElementById('choices').append(choice);
+    }
+}
+
+function selectChoice () {
+    you = this.id;
+    document.getElementById('your-choice').src = you + '.jpg';
+
+    // random for computer
+
+    computer = choices[Math.floor(Math.random() * choices.length)]; 
+    document.getElementById('computers-choice').src = computer + '.jpg'
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    let buttons = document.getElementsByTagName('button');
-
-    for (let button of buttons) {
-        button.addEventListener('click', function () {
-            if (this.getAttribute('data-type') === 'submit') {
-                let gameType = this.getAttribute('data-type');
-                runGame(gameType);
+    // check for winner
+    if (you == computer) {
+        yourScore += 1;
+        computerScore += 1;
+  } 
+    else {
+        if (you == 'rock') {
+            if (computer == 'scissors' || computer == 'lizard') {
+                yourScore +=1;
             }
-        })
+            else {
+                computerScore += 1;
+            }
+        }
+        
+        if (you == 'paper') {
+            if (computer == 'rock' || computer == 'spock') {
+                yourScore +=1;
+            }
+            else {
+                computerScore += 1;
+            }
+        }
+        if (you == 'scissors') {
+            if (computer == 'paper' || computer == 'lizard') {
+                yourScore +=1;
+            }
+            else {
+                computerScore += 1;
+            }
+        }
+        
+        if (you == 'lizard') {
+            if (computer == 'spock' || computer == 'paper') {
+                yourScore +=1;
+            }
+            else {
+                computerScore += 1;
+            }
+        }
+        if (you == 'spock') {
+            if (computer == 'scissors' || computer == 'rock') {
+                yourScore +=1;
+            }
+            else {
+                computerScore += 1;
+            }
+        }
 
+
+        
     }
 
-    /**
- * The main game 'loop', called when the crups is first loaded
- * and after the user's answer has been processed
- */
-function runGame (event) {
-
-    // Creates two random numbers between 1 and 5
-    let num1 = Math.floor(Math.random() * 5) + 1;
-    let num2 = Math.floor(Math.random() * 5) + 1;
-    let num3 = Math.floor(Math.random() * 5) + 1;
-    let num4 = Math.floor(Math.random() * 5) + 1;
-    let num5 = Math.floor(Math.random() * 5) + 1;
-
-    if (event === 'num1') {
-        (num1, num2, num3, num4, num5);
-
-}
-}
-
-
-/**
- * Gets the current score from the DOM and increments it by 1
- */
- function incrementScore () {
-
-    let oldScore = parseInt(document.getElementById('wins').innerText);
-    document.getElementById('wins').innerText = ++oldScore;
-}
-
-/**
- * Gets the current tally of incorrect answers from the DOM and increments it by 1
- */
- function incrementFail () {
-    let oldScore = parseInt(document.getElementById('losses').innerText);
-    document.getElementById('losses').innerText = ++oldScore;
-}
-
-
-function onclick () {
-    let lockOn = (document.getElementsByClassName('r'))
-}
+    document.getElementById('your-score').innerText = yourScore;
+    document.getElementById('computers-score').innerText = computerScore;
+  }
