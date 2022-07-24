052d9d4cd8ed0ec49de7424cbfd995777c66f0a7
LadyNeowen <Alicia.Westerlund@outlook.com>
2022-07-24 12:17:52 +0000

    successfully added gameover function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,13 @@ window.onload = function () {
     }
 }
 
-function selectChoice () {
+function selectChoice() {
     you = this.id;
     document.getElementById('your-choice').src = you + '.jpg';
 
     // random for computer
 
-    computer = choices[Math.floor(Math.random() * choices.length)]; 
+    computer = choices[Math.floor(Math.random() * choices.length)];
     document.getElementById('computers-choice').src = computer + '.jpg'
 
 
@@ -32,100 +32,76 @@ function selectChoice () {
     if (you == computer) {
         yourScore += 0;
         computerScore += 0;
-  } 
-    else {
+    } else {
         if (you == 'rock') {
             if (computer == 'scissors' || computer == 'lizard') {
-                yourScore +=1;
+                yourScore += 1;
                 document.getElementById('your-choice').classList.add('winner');
-                
-            }
-            else {
+
+            } else {
                 computerScore += 1;
                 document.getElementById('computers-choice').classList.add('winner');
 
             }
         }
-        
+
         if (you == 'paper') {
             if (computer == 'rock' || computer == 'spock') {
-                yourScore +=1;
+                yourScore += 1;
                 document.getElementById('your-choice').classList.add('winner');
-                
-            }
-            else {
+
+            } else {
                 computerScore += 1;
                 document.getElementById('computers-choice').classList.add('winner');
-    
+
             }
         }
         if (you == 'scissors') {
             if (computer == 'paper' || computer == 'lizard') {
-                yourScore +=1;
+                yourScore += 1;
                 document.getElementById('your-choice').classList.add('winner');
-                
-            }
-            else {
+
+            } else {
                 computerScore += 1;
                 document.getElementById('computers-choice').classList.add('winner');
-                
+
             }
         }
-        
+
         if (you == 'lizard') {
             if (computer == 'spock' || computer == 'paper') {
-                yourScore +=1;
+                yourScore += 1;
                 document.getElementById('your-choice').classList.add('winner');
-                
-            }
-            else {
+
+            } else {
                 computerScore += 1;
                 document.getElementById('computers-choice').classList.add('winner');
-                
+
             }
         }
         if (you == 'spock') {
             if (computer == 'scissors' || computer == 'rock') {
-                yourScore +=1;
+                yourScore += 1;
                 document.getElementById('your-choice').classList.add('winner');
-                
-            }
-            else {
+
+            } else {
                 computerScore += 1;
                 document.getElementById('computers-choice').classList.add('winner');
-                
+
 
             }
-           /* function gameOver () {
-                if (computerScore > 6);
-                alert('Game Over!') {
-                    else if (yourScore > 6;)
-                    alert('Game Over!')
-                }
-            }
-            */
+        }
+
 
     }
-    
-
-}
 
 
     document.getElementById('your-score').innerText = yourScore;
     document.getElementById('computers-score').innerText = computerScore;
-  }
+    gameOver ();
+}
 
-
-
- /* for (let turns = 0; turns < 6; turns++); if (turns == 6) { 
-    alert(message: Game Over); 
-} */
-
-
-/*function gameOver () {
-var paragraph = document.getElementById("winner");
-var text = document.createTextNode("Oh would you look at that? You actually won!");
-
-paragraph.appendChild(text);
-
-}*/
+    function gameOver () {
+        if (computerScore > 3) alert ('Game Over! Unsurprisingly, you have lost.');
+        else if (yourScore > 3) alert ('Game Over! Oh would you look at that? You actually won. Kudos.')
+    }
